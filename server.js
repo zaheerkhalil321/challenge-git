41351955213bd4b6b25d7641354a07dc2c2e8998
David Guttman <david@davidguttman.com>
2021-04-18 22:22:44 -0700

    * feat: add user-agent endpoint

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
-const http = require('http')
+const http = require("http");
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  if (req.url === '/') return respondHello(req, res)
-  if (req.url === '/user-agent') return respondUserAgent(req, res)
+  if (req.url === "/") return respondHello(req, res);
+  if (req.url === "/user-agent") return respondUserAgent(req, res);
 
-  res.end()
-})
+  res.end();
+});
 
-function respondHello (req, res) {
-  res.end(JSON.stringify({ msg: 'hello' }))
+function respondHello(req, res) {
+  res.end(JSON.stringify({ msg: "hello" }));
 }
 
-function respondUserAgent (req, res) {
-  const ua = req.headers['user-agent']
-  res.end(JSON.stringify({ ua }))
+function respondUserAgent(req, res) {
+  const ua = req.headers["user-agent"];
+  res.end(JSON.stringify({ ua }));
 }
 
-server.listen(PORT)
-console.log(`Server listening on port ${PORT}`)
+server.listen(PORT);
+console.log(`Server listening on port ${PORT}`);
 
-if (require.main !== module) module.exports = server
+if (require.main !== module) module.exports = server;
